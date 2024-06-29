233eac06013a5de541d3ea50a5d3f73b466cb71e
sidulanaksekolahan <mirfan.duri@gmail.com>
2024-06-30 02:31:51 +0700

    Testing notification for slack

--- src/test/java/com/nucsaping/SpringbootMysqlSlackGithubactionApplicationTests.java
+++ src/test/java/com/nucsaping/SpringbootMysqlSlackGithubactionApplicationTests.java
@@ -1,14 +1,12 @@
 package com.nucsaping;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootMysqlSlackGithubactionApplicationTests {
 
-	@Disabled
 	@Test
 	void contextLoads() {
 		String x = "x";
